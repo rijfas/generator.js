7721b55b9386285a29bd03e98e07b316918fd182
amjadcp <amjadcp03@gmail.com>
2025-02-22 20:25:30 +0530

    fix: import error

--- src/router.js
+++ src/router.js
@@ -2,13 +2,9 @@ import { Router } from "express";
 import { healthCheck } from "./controllers/app.controller.js";
 import productRouter from "./apps/product/product.router.js";
 import adminRouter from "./apps/admin/admin.router.js";
-import sampleRouter from "./apps/sample/sample.router.js";
 
 
 const router = Router();
-
-router.use("/samples", sampleRouter);
-
 
 // common routes
 router.get("/health", healthCheck);
